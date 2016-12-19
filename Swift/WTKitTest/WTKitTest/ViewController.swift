bdcaaec71be54c6e962eb07f0313d003bad8170d
songwentong <songwentong@mobimagic.com>
2016-12-19 13:28:01 +0800

    用pod测试一下WTKit

--- Swift/WTKitTest/WTKitTest/ViewController.swift
+++ Swift/WTKitTest/WTKitTest/ViewController.swift
@@ -13,6 +13,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let task = WTKit.dataTask(with: "https://www.apple.com")
+        task.completionHandler = {(data,response,error)in
+            print("handle response/error")
+        }
+        task.stringHandler = {(string,error)in
+            print("handle string/error")
+        }
+        task.jsonHandler = {(a,b)in
+            print("handle json/error")
+        }
+        task.progressHandler = {(n1,n2)in
+            print("handle progress")
+        }
     }
 
     override func didReceiveMemoryWarning() {
