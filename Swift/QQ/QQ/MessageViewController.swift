1b4d9efec842e836cd67115980cbada41683476a
songwentong <songwentong@mobimagic.com>
2016-12-22 16:37:11 +0800

    创建项目

---
+++ Swift/QQ/QQ/MessageViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MessageViewController.swift
+//  QQ
+//
+//  Created by SongWentong on 22/12/2016.
+//  Copyright © 2016 songwentong. All rights reserved.
+//
+
+import UIKit
+
+public class MessageViewController: UIViewController {
+
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override public func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
