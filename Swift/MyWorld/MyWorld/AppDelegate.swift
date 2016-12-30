5517037dff1641f756e4afd2c58e6f21f851a1ae
songwentong <songwentong@mobimagic.com>
2016-12-30 11:47:59 +0800

    mine craft

---
+++ Swift/MyWorld/MyWorld/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  MyWorld
+//
+//  Created by SongWentong on 30/12/2016.
+//  Copyright (c) 2016 songwentong. All rights reserved.
+//  study mine craft
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
