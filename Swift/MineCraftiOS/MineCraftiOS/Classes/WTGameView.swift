40fa4632e27b52f165e72d44e61bccd88227b9cc
songwentong <songwentong@mobimagic.com>
2016-12-30 18:17:43 +0800

    摄像机跟着方块动

---
+++ Swift/MineCraftiOS/MineCraftiOS/Classes/WTGameView.swift
@@ -0,0 +1,28 @@
+//
+//  WTGameView.swift
+//  MineCraftiOS
+//
+//  Created by SongWentong on 30/12/2016.
+//  Copyright © 2016 songwentong. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+class WTGameView: SCNView {
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    func configView(){
+        
+    }
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- Swift/MineCraftiOS/MineCraftiOS/GameViewController.swift
+++ Swift/MineCraftiOS/MineCraftiOS/GameViewController.swift
@@ -12,8 +12,10 @@ import SceneKit
 
 class GameViewController: UIViewController {
 
+    @IBOutlet var scnView: WTGameView!
     var cameraNode:SCNNode?
     var label:UILabel?
+    var tapGesture:UITapGestureRecognizer?
     public func readJSON()->Void{
     
     }
@@ -25,7 +27,9 @@ class GameViewController: UIViewController {
         super.viewDidLoad()
         
         setupScene()
-        addBlock(with: "WoodCubeA.jpg", at: SCNVector3Make(10, 10, 10))
+        let block = addBlock(with: "WoodCubeA", at: SCNVector3Make(10, 10, 100))
+        block?.addChildNode(cameraNode!)
+        scnView.pointOfView = cameraNode
         setUpCameraTimer()
     }
     func setUpCameraTimer(){
@@ -37,20 +41,33 @@ class GameViewController: UIViewController {
          _ = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(GameViewController.timerMethod(timer:)), userInfo: nil, repeats: true)
     }
     func timerMethod(timer:Timer){
-        label?.text = "camera: \(cameraNode?.camera?.projectionTransform)"
+//        cameraNode?.eulerAngles
+        if let scnView:SCNView = self.view as! SCNView? {
+            label?.text = "camera: \(scnView.pointOfView?.eulerAngles)"
+            print("camera: \(scnView.pointOfView?.eulerAngles)")
+        }
+
     }
     func setupScene(){
+        
+//        self.scnView.pointOfView = SCNNode()
         // create a new scene
         let scene = SCNScene()
         
         // create and add a camera to the scene
         let cameraNode = SCNNode()
         cameraNode.camera = SCNCamera()
-        scene.rootNode.addChildNode(cameraNode)
+        cameraNode.camera?.zFar = 500;
+        cameraNode.position = SCNVector3Make(0, 0, 0);
+        cameraNode.eulerAngles = SCNVector3Make(3, 0.6, 3)
+//        cameraNode.rotation  = SCNVector4Make(1, 1, 1, Float(-M_PI*0.75));
+//        scene.rootNode.addChildNode(cameraNode)
         
         // place the camera
-        cameraNode.position = SCNVector3(x: 20, y: 20, z: 25)
         self.cameraNode = cameraNode
+//        self.scnView.pointOfView = cameraNode
+        
+        
         // create and add a light to the scene
         let lightNode = SCNNode()
         lightNode.light = SCNLight()
@@ -96,7 +113,7 @@ class GameViewController: UIViewController {
         let floor = SCNNode()
         floor.geometry = SCNFloor()
         floor.geometry?.firstMaterial?.diffuse.contents = "wood.png"
-        floor.geometry?.firstMaterial?.diffuse.contentsTransform = SCNMatrix4MakeScale(2, 2, 1)
+//        floor.geometry?.firstMaterial?.diffuse.contentsTransform = SCNMatrix4MakeScale(2, 2, 1)
         floor.geometry?.firstMaterial?.locksAmbientWithDiffuse = true
         let body = SCNPhysicsBody.static()
         floor.physicsBody = body
@@ -104,19 +121,20 @@ class GameViewController: UIViewController {
             view.scene?.rootNode.addChildNode(floor)
         }
     }
-    func addBlock(with imageName:String,at position:SCNVector3){
-        if let view:SCNView = self.view as! SCNView? {
-            if let scene:SCNScene = view.scene {
-                let block = SCNNode()
-                block.position = position
-                block.geometry = SCNBox.init(width: 5, height: 5, length: 5, chamferRadius: 0)
-                block.geometry?.firstMaterial?.diffuse.contents = imageName
-                block.geometry?.firstMaterial?.diffuse.mipFilter = .linear
-                block.physicsBody = SCNPhysicsBody.dynamic()
-                scene.rootNode.addChildNode(block)
-            }
-        }
+    func addBlock(with imageName:String,at position:SCNVector3)->SCNNode?{
         
+        if let scene:SCNScene = self.scnView.scene {
+            let block = SCNNode()
+            block.position = position
+            block.geometry = SCNBox.init(width: 5, height: 5, length: 5, chamferRadius: 0)
+            block.geometry?.firstMaterial?.diffuse.contents = imageName
+            block.geometry?.firstMaterial?.diffuse.mipFilter = .linear
+            block.physicsBody = SCNPhysicsBody.dynamic()
+            block.eulerAngles = SCNVector3.init(M_PI, 0, 0)
+            scene.rootNode.addChildNode(block)
+            return block
+        }
+        return nil
     }
     func handleTap(_ gestureRecognize: UIGestureRecognizer) {
         // retrieve the SCNView
